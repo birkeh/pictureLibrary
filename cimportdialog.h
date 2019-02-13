23159be6c9ebcbb9bfdb8aa6dda0eb25bc37a75f
Herwig Birke <Herwig.Birke@windesign.at>
2019-02-13 18:36:49 +0100

    initial commit

--- cimportdialog.h
+++ cimportdialog.h
@@ -12,12 +12,25 @@ namespace Ui {
 class cImportDialog;
 }
 
+/**
+ * @brief
+ *
+ */
 class cImportDialog : public QDialog
 {
 	Q_OBJECT
 
 public:
+	/**
+	 * @brief
+	 *
+	 * @param parent
+	 */
 	explicit cImportDialog(QWidget *parent = nullptr);
+	/**
+	 * @brief
+	 *
+	 */
 	~cImportDialog();
 
 private slots:
@@ -48,17 +61,35 @@ private slots:
 	void					onThumbnailSelected(const QItemSelection& selection, const QItemSelection& previous);
 
 private:
-	Ui::cImportDialog*		ui;
+	Ui::cImportDialog*		ui;										/**< TODO: describe */
 	QStandardItemModel*		m_lpImportListModel;					/**< TODO: describe */
 
 	void					initUI();
 	void					createActions();
 
+	/**
+	 * @brief
+	 *
+	 * @param szPath
+	 * @param bRecursive
+	 */
 	void					readDirectory(const QString& szPath, bool bRecursive);
 
+	/**
+	 * @brief
+	 *
+	 */
 	void					accept();
+	/**
+	 * @brief
+	 *
+	 */
 	void					reject();
 
+	/**
+	 * @brief
+	 *
+	 */
 	void					savePosition();
 protected:
 	/*!
--- ctoolboxinfo.h
+++ ctoolboxinfo.h
@@ -11,17 +11,35 @@ namespace Ui {
 class cToolBoxInfo;
 }
 
+/**
+ * @brief
+ *
+ */
 class cToolBoxInfo : public QWidget
 {
 	Q_OBJECT
 
 public:
+	/**
+	 * @brief
+	 *
+	 * @param parent
+	 */
 	explicit cToolBoxInfo(QWidget *parent = nullptr);
+	/**
+	 * @brief
+	 *
+	 */
 	~cToolBoxInfo();
 
+	/**
+	 * @brief
+	 *
+	 * @param lpPicture
+	 */
 	void				setPicture(cPicture* lpPicture);
 private:
-	Ui::cToolBoxInfo*	ui;
+	Ui::cToolBoxInfo*	ui; /**< TODO: describe */
 };
 
 #endif // CTOOLBOXINFO_H
